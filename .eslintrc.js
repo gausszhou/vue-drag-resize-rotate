d3f938d18eeba779408ac393d5953e1b1f098187
gausszhou <gausszhou@qq.com>
2021-08-04 02:28:11 +0800

    chore: 发布到npm; version-2.0.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,17 @@ module.exports = {
   env: {
     node: true
   },
+  // extends: ['plugin:vue/recommended', 'eslint:recommended'], // 官方推荐，比较严格
+  extends: ["plugin:vue/essential", "eslint:recommended"], // 基本的
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-unused-vars': 'off',
-    'no-unreachable': 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-unused-vars": "off",
+    "no-unreachable": "off"
   },
   parserOptions: {
-    'parser': 'babel-eslint',
-    "ecmaVersion": 6,
-    "sourceType": "module"
-  },
-}
+    parser: "babel-eslint",
+    ecmaVersion: 6,
+    sourceType: "module"
+  }
+};
